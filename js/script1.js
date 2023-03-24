e1dbcc7db60d994d22ec4094daf01c6a1671114c
Jesse <kayjes00@gmail.com>
2023-03-24 14:16:10 +0200

    updated about.html file

--- js/script1.js
+++ js/script1.js
@@ -93,13 +93,29 @@ for (let i = num +1; i < speakers.length; i += 1){
 const button = document.querySelector('#btn');
 button.addEventListener('click', () => {
   modal.classList.toggle('hidebtn');
-  button.classList.add('hidebtn');
-  // button.classList.remove('#btn');
+  button.classList.add('hideme');
+  button.classList.remove('btn-speakers');
 });
 
 
 
 
+// menu
+
+/* eslint-enable */
+
+const mobileMenu = document.querySelector('.main-menu');
+const closeBtn = document.getElementById('close');
+const hamburger = document.getElementById('menu-bar');
+
+hamburger.onclick = function showMenu() {
+  document.body.style.overflow = 'hidden';
+  mobileMenu.style.display = 'block';
+};
+closeBtn.onclick = function closeMenu() {
+  document.body.style.overflow = 'visible'; /* eslint-disable */ 
+  mobileMenu.style.display = none = 'none'; /* eslint-disable */ 
+};
 
 
 
@@ -107,80 +123,4 @@ button.addEventListener('click', () => {
 
 
 
-// /*eslint-disable */
 
-// const speakersData = [
-//     // {
-//     //   photo: './images/one.jpg',
-//     //   name: 'Hellen john',
-//     //   title: 'CE0 of African movies Festival',
-//     //   description:
-//     //     'Founder of the African movies festival, with 15 years experience in film making',
-//     // },
-//     // {
-//     //   photo: './images/two.jpg',
-//     //   name: 'Bazu Bazu',
-//     //   title: 'CE0 of African movies Festival',
-//     //   description:
-//     //     'Founder of the African movies festival, with 15 years experience in film making',
-//     // },
-//     {
-//       photo: './images/three.jpg',
-//       name: 'Kevin Mzito',
-//       title: 'CE0 of African movies Festival',
-//       description:
-//         'Founder of the African movies festival, with 15 years experience in film making',
-//     },
-//     {
-//       photo: './images/four.jpg',
-//       name: 'Felix Mtongoi',
-//       title: 'CE0 of African movies Festival',
-//       description:
-//         'Founder of the African movies festival, with 15 years experience in film making',
-//     },
-  
-//     {
-//       photo: './images/five.jpg',
-//       name: 'Linda Henrys',
-//       title: 'CE0 of African movies Festival',
-//       description:
-//         'Founder of the African movies festival, with 15 years experience in film making',
-//     },
-//     {
-//       photo: './images/six.jpg',
-//       name: 'Shruti Shah',
-//       title: 'CE0 of African movies Festival',
-//       description:
-//         'Founder of the African movies festival, with 15 years experience in film making',
-//     },
-//   ];
-  
-//   const speakerDetails = document.querySelector('.speaker-area');
-  
-//   speakersData.forEach((speaker) => {
-//     speakerDetails.innerHTML += `<article class='speakers-detail'>
-//             <img src="${speaker.photo}" class="image" ></img>
-//           <div class='col-2'>
-//             <h3>${speaker.name}</h3>
-//             <p style="color:#ed5342"><em>${speaker.title}</em></p>
-//             <span></span>
-//             <p>
-//             ${speaker.description} 
-//             </p>
-//           </div>
-//           </article> `;
-//   });
-  
-  
-  
-//   const mobileMenu = document.querySelector('.main-menu');
-//   const closeBtn = document.getElementById('close');
-//   const hamburger = document.getElementById('menu-bar');
-  
-//   hamburger.onclick = function showMenu() {
-//     mobileMenu.style.display = 'block';
-//   };
-//   closeBtn.onclick = function closeMenu() {
-//     mobileMenu.style.display = none = 'none';
-//   };
-//   /* eslint-enable */
--- js/script2.js
+++ js/script2.js
@@ -5,8 +5,10 @@ const closeBtn = document.getElementById('close');
 const hamburger = document.getElementById('menu-bar');
 
 hamburger.onclick = function showMenu() {
+  document.body.style.overflow = 'hidden';
   mobileMenu.style.display = 'block';
 };
-closeBtn.onclick = function closeMenu() { /* eslint-disable */ 
+closeBtn.onclick = function closeMenu() {
+  document.body.style.overflow = 'visible'; /* eslint-disable */ 
   mobileMenu.style.display = none = 'none'; /* eslint-disable */ 
 };
